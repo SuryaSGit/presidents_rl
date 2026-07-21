fce636656b92ed754e247d5259ea15fe0269a194
SuryaSGit <classica314@gmail.com>
2026-07-21 15:40:58 +0530

    update nn

--- test_rl.py
+++ test_rl.py
@@ -4,5 +4,88 @@ from typing import Optional
 import numpy as np
 import gymnasium as gym
 from gymnasium.utils.env_checker import check_env
-import environment
+import torch
+from torch import nn
+from collections import deque
+import itertools
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+BUFFER_SIZE = 50000
+MIN_REPLAY_SIZE = 1000
+EPSILON_START = 1.0
+EPSILON_END = 0.02
+EPSILON_DECAY = 10000
+TARGET_UPDATE_FREQ = 1000
+
+class Network(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    def act(self,obs):
+        obs_t = torch.as_tensor(obs, dtype=torch.float32).unsqueeze(0)
+        q_values = self(obs_t.unsqueeze(0))
+        return q_values
+
 env = gym.make("gymnasium_env/presidents-rl")
+replay_buffer = deque(maxlen=BUFFER_SIZE)
+rew_buffer = deque([0.0],maxlen=100)
+
+episode_reward = 0.0
+
+online_net = Network(108,92)
+target_net = Network(108,92)
+target_net.load_state_dict(online_net.state_dict())
+
+obs =  env.reset()[0]
+for _ in range(MIN_REPLAY_SIZE):
+    action = env.action_space_sample()
+    new_obs, reward, terminated, truncated, info = env.step(action)
+    replay_buffer.append((obs, action, reward, terminated, new_obs))
+    obs = new_obs
+    if terminated:
+        obs = env.reset()[0]
+#main training
+
+obs = env.reset()[0]
+for step in itertools.count():
+    epsilon = np.interp(step, [0, EPSILON_DECAY], [EPSILON_START, EPSILON_END])
+    rnd_sample = random.random()
+    if(rnd_sample < epsilon):
+        action = env.action_space_sample()
+    else:
+        q_vals = online_net.act(obs)
+        action = env.getaction(q_vals)
+    new_obs, reward, terminated, truncated, info = env.step(action)
+    replay_buffer.append((obs, action, reward, terminated, new_obs))
+    obs = new_obs
+    episode_reward += reward    
+    if terminated:
+        obs = env.reset()[0]    
+        episode_reward = 0.0
+        rew_buffer.append(episode_reward)
+
+    transitions = random.sample(replay_buffer, BATCH_SIZE)
+
+    obses = np.asarray([t[0] for t in transitions])
+    actions = np.asarray([t[1] for t in transitions])
+    rews = np.asarray([t[2] for t in transitions])
+    dones = np.asarray([t[3] for t in transitions])
+    newobses = np.asarray([t[4] for t in transitions])
+
+    obses_t = torch.as_tensor(obses,dtype = torch.float32)
+    actions_t = torch.as_tensor(actions,dtype = torch.int64).unsqueeze(-1)
+    rews_t = torch.as_tensor(rews,dtype = torch.float32).unsqueeze(-1)
+    dones_t = torch.as_tensor(dones,dtype = torch.float32).unsqueeze(-1)
+    newobses_t   = torch.as_tensor(newobses,dtype = torch.float32)
+
+    target_q_values 
